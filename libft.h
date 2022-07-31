599b7bd2d5505a33e6c96cfa72f89aba5708abc9
Abdelmouanimhmamed <mounaimhmamed@gmail.com>
2022-07-31 13:08:28 +0100

    make it better

--- libft.h
+++ libft.h
@@ -33,4 +33,5 @@ void	ft_putnbr_fd(int n, int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_itoi(int n);
 void	*ft_memmove(void *dest, const void *src, size_t	n);
+int	ft_isascii(int c);
 #endif
