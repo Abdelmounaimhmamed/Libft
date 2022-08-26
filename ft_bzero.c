f6fda19c1d34319cdeb2de3ffd3b40d9ebdc395d
ABDELMOUNAIM HMAMED <79320947+Abdelmounaimhmamed@users.noreply.github.com>
2022-08-26 19:16:39 +0100

    make it better

--- ft_bzero.c
+++ ft_bzero.c
@@ -4,3 +4,21 @@ void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s,'\0',n);
 }
+/*
+int     main(int argc , char **argv)
+{
+        ft_bzero(argv[1],7);
+        size_t  i;
+
+         i = 0;
+         while (argv[1][i] == '\0')
+                 i++;
+         while (argv[1][i] != '\0')
+         {
+                 printf("%c",argv[1][i]);
+                 i++;
+         }
+         return 0;
+	
+}
+*/
